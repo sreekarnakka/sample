312f7ee0d603976578dba62f6a3a5f0d1101dc58
A Sai Prashanth <95900559+saiprashanth88@users.noreply.github.com>
2023-10-13 12:54:44 +0530

    Update settings.py

--- sir/myproject/myproject/settings.py
+++ sir/myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(kl80eq_of)5#-l7zcxtn5iqm7h3yim$n4c8**unl%*b*lzmvi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','now.sh']
 
 
 # Application definition
@@ -129,10 +129,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 # STATIC_URL = 'static/'
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR,"static"]
+
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
