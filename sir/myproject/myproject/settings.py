9bfdc45c309d661ba2be3ee3cb9336a12a179c3b
sreekarnakka <112407085+sreekarnakka@users.noreply.github.com>
2023-10-14 09:31:08 +0530

    Update settings.py

--- sir/myproject/myproject/settings.py
+++ sir/myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(kl80eq_of)5#-l7zcxtn5iqm7h3yim$n4c8**unl%*b*lzmvi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','.pythonanywhere.com']
 
 
 # Application definition
